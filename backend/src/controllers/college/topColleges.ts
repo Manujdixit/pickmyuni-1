830cfecc0658d392233d638960455cd5c696034b
Manujdixit <manujdixit04@gmail.com>
2025-07-08 11:33:35 +0530

    feat: Add tagSanatize utility function and integrate it into getTopColleges for slug sanitization

--- backend/src/controllers/college/topColleges.ts
+++ backend/src/controllers/college/topColleges.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { prisma } from "../../lib/prisma";
+import { tagSanatize } from "../../utils/tagsanatize";
 
 /**
  * @swagger
@@ -75,6 +76,7 @@ export const getTopColleges = async (req: Request, res: Response) => {
         colleges: colleges.map((college: any) => ({
           ...college,
           count_collegewise_course: college._count.CollegesCourses,
+          slug: tagSanatize(college.slug),
         })),
         streams: [
           { id: 0, name: "All" },
